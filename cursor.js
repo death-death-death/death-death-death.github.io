6d464ce9c2cc41462a60dcc0063f8c7675c3a9fb
death-death-death <nampatnampat@outlook.com>
2025-11-07 02:15:51 +0500

    new stuff

--- cursor.js
+++ cursor.js
@@ -1,31 +1,29 @@
-// Находим наш кастомный элемент курсора
+// Находим элементы кастомного курсора
 const customCursor = document.getElementById('custom-cursor');
-// Находим элемент, при наведении на который курсор должен стать анимированным
-const targetLink = document.querySelector('a');
-
+const targetLinks = document.querySelectorAll('.gif-link');
 
 // ******************************************************
 // ФУНКЦИЯ ПЕРЕМЕЩЕНИЯ КУРСОРА ЗА МЫШЬЮ
 // ******************************************************
 document.addEventListener('mousemove', (e) => {
-    // Устанавливаем позицию кастомного курсора
     customCursor.style.left = `${e.clientX}px`;
     customCursor.style.top = `${e.clientY}px`;
 });
 
 
 // ******************************************************
-// ФУНКЦИЯ СМЕНЫ КУРСОРА ПРИ НАВЕДЕНИИ НА ССЫЛКУ
+// ФУНКЦИЯ СМЕНЫ КУРСОРА (КЛАССОВ) ПРИ НАВЕДЕНИИ НА ССЫЛКИ
 // ******************************************************
+targetLinks.forEach(link => {
+    
+    link.addEventListener('mouseover', () => {
+        // Добавляем класс, который меняет фон на Link.webp
+        customCursor.classList.add('cursor-link');
+    });
 
-// 1. При наведении на ссылку (<a>)
-targetLink.addEventListener('mouseover', () => {
-    // Добавляем класс, который в CSS меняет фон на анимированный GIF
-    customCursor.classList.add('cursor-animated'); 
-});
-
-// 2. Когда мышь уходит со ссылки (<a>)
-targetLink.addEventListener('mouseout', () => {
-    // Убираем класс, чтобы вернуть статичный PNG по умолчанию
-    customCursor.classList.remove('cursor-animated'); 
+    link.addEventListener('mouseout', () => {
+        // Удаляем класс, возвращаясь к Arrow.webp
+        customCursor.classList.remove('cursor-link');
+    });
+    
 });
